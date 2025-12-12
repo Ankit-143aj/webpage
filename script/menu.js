37b0bfb1dba66b1e3fe6b68985681b6f978c9a19
Ankit-143aj <sankit9127@gmail.com>
2025-12-12 15:05:30 +0530

    Update menu.js

--- script/menu.js
+++ script/menu.js
@@ -1,1 +1,24 @@
 
+
+      fetch("navbar.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("nav").innerHTML = data;
+
+        let currentPage = window.location.pathname.split("/").pop();
+        //menu me anchor tag tak pahuchne ka tarika
+        let links = document.querySelectorAll(".menu .menubar ul li a");
+
+        if(!currentPage || currentPage == "#"){
+            currentPage="index.html";
+        }
+        //har anchor tag ko process karege
+        links.forEach(link => {
+          console.log("currentPage=" , currentPage);
+          let linkPage =  link.getAttribute("href");
+          
+          if(linkPage == currentPage){
+            link.closest("li").classList.add("underline_current");
+          }
+        });
+      });
